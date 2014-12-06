26068fbe6f2c98470b2d4524abec2ed4151574ca
Cajs <camcajs@gmail.com>
2014-12-06 18:29:06 +0000

    Fix typing error.

--- scripts.tcl
+++ scripts.tcl
@@ -1,6 +1,6 @@
 # Path Configuration
 set bot(scripts) "/home/ircbot/IRCBot/resources/scripts"
-set bot(resources) "/home/ircbot/IRCCBot/resources/"
+set bot(resources) "/home/ircbot/IRCBot/resources/"
 # Loading scripts (Remove/Add scripts to the array to load them on start-up)
 set scripts {
 	"iptools.tcl"
